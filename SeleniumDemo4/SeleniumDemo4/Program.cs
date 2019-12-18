3010a882dbc7af99b95d6f1be7ded170b681f4c9
ajay.devopsenablers <ajay.devopsenablers@outlook.com>
2019-12-18 12:39:22 +0000

    static main function added

--- SeleniumDemo4/SeleniumDemo4/Program.cs
+++ SeleniumDemo4/SeleniumDemo4/Program.cs
@@ -7,16 +7,11 @@ namespace SeleniumDemo4
 {
     class Program
     {
-        IWebDriver driver;
-     [SetUp]
-     public void lauchBrowser()
+        static IWebDriver driver;
+     public static void Main ()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-        }
-        [Test]
-        public void openURL()
-        {
             driver.Navigate().GoToUrl("https://www.twitter.com");
         }
     }
