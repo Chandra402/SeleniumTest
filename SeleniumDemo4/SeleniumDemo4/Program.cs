b1da840db334b1d497b7107edc13567bf256d50f
ajay.devopsenablers <ajay.devopsenablers@outlook.com>
2019-12-18 10:53:19 +0000

    commit one for all files

---
+++ SeleniumDemo4/SeleniumDemo4/Program.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumDemo4
+{
+    class Program
+    {
+        IWebDriver driver;
+     [SetUp]
+     public void lauchBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+        [Test]
+        public void openURL()
+        {
+            driver.Navigate().GoToUrl("https://www.twitter.com");
+        }
+    }
+}
